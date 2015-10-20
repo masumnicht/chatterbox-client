c3e217cb8177a2ca7d00126237603c7d05240bce
masumnicht <masumnicht@gmail.com>
2015-10-19 19:20:54 -0700

    Basic functionality.  Bad GUI.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,132 @@
 // YOUR CODE HERE:
+var app = {
+  server: 'https://api.parse.com/1/classes/chatterbox'
+};
+
+var currentTime = Date.now();
+var username = window.location.search.replace('?username=', '');
+var room = 'default';
+
+app.init = function() {
+
+}
+
+/*var message = {
+  username: 'shawndrost',
+  text: 'trololo',
+  roomname: '4chan'
+};*/
+app.send = function(message) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: this.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.fetch = function() {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: this.server,
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+
+      
+      var i = 0;
+      var tempTime = currentTime;
+      currentTime = Date.now();
+      while(Date.parse(data.results[i].createdAt) > tempTime && i < 500) {
+
+        i++;
+      }
+      /*if(i === 0){
+        debugger;
+      }*/
+      i--;
+      while(i >=0) {
+        if (data.results[i].text === undefined){
+          data.results[i].text = '';
+        }
+        if (data.results[i].room === undefined){
+          data.results[i].room = '';
+        }
+        if (data.results[i].username === undefined){
+          data.results[i].username = '';
+        }
+        var msg = $('<span display="in-line">' + 'msg:' + escapedString(data.results[i].text) + '</span>')
+        var name = $('<div>' + 'name:' + escapedString(data.results[i].username) + '</div>')
+        $('#chats').prepend(name);
+        $('#chats').prepend(msg);
+        $('#chats').prepend($('<span display="in-line">' + 'time:' + data.results[i].createdAt + '</span>'))
+        $('#chats').prepend($('<span display="in-line">' + '--------------------------------------' + '</span>'))
+        i--;
+      }
+      //currentTime = Date.now();
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to get message');
+    }
+  });
+}
+app.fetch();
+setInterval(function() {
+  app.fetch()
+}, 1000);
+
+$(document).ready(function(){
+  $(".button").on('click', function(event){
+    event.preventDefault();
+    var message = {
+      username: username,
+      room: room,
+      text: $('form')[0][0].value
+    }
+    app.send(message);
+    // $('form')[0][0].value = '';
+  });
+});
+
+
+var escapedString = function(str){
+  var newStr;
+  if (str === undefined){
+    debugger;
+  }
+  newStr = str.replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/\"/g, '&quot;')
+      .replace(/\'/g, '&#39;');
+  return newStr;
+};
+
+app.clearMessages = function() {
+  $('#chats').text = ''
+}
+
+app.addMessages = function() {
+
+}
+
+app.addRoom = function() {
+
+}
+
+app.addFriend = function() {
+
+}
+
+app.handleSubmit = function() {
+
+}
